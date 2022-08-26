b3b92c9fc170974765df9cd9bbd254507fe1b4b2
Ubuntu <ubuntu@ip-172-31-15-157.us-west-1.compute.internal>
2022-08-26 16:52:56 +0000

    db table columns count compare util-20220827

---
+++ etl/query-cols.js
@@ -0,0 +1,25 @@
+
+let db = require('../models')
+const LOGGER=console.log
+
+const main=async _=>{
+	let resptables =await	db.sequelize.query(`show tables`) // .then(resp0=>{resp=resp0;console.log(resp[0].length )} )
+
+	let list = resptables[0].map(elem=>elem['Tables_in_nipsethtestnet02'] ).sort ( (a,b)=>a.localeCompare ( b))
+//	let list = resptables[0].map(elem=>elem['Tables_in_nipsethtestnet02'] ).sort ( (a,b)=>a > b? +1:-1)
+//	resptables[0].forEach ( async ( elem  , idx ) =>{
+//		let { Tables_in_nipsethtestnet02 : tablename} = elem
+//		let { Tables_in_nipsethtestnet02 : tablename} = elem
+	list.forEach ( async ( elem  , idx ) =>{
+		false && LOGGER( elem )
+		let tablename = elem
+ 		let respcolcount = await db.sequelize.query(`desc ${tablename} `) // .then(resp0=>{resp=resp0;console.log(resp[0].length )} )
+		LOGGER( idx , tablename , respcolcount[0].length ) 
+	})
+}
+main()
+
+// db.sequelize.query(`desc users`).then(resp0=>{resp=resp0;console.log(resp[0].length )} )
+
+
+
